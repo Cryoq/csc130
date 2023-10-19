c51bd9063197cfc9f7b50a4256fac9901bbf1a61
Cryoq <99939677+Cryoq@users.noreply.github.com>
2023-10-19 13:22:49 -0500

    Update LEDBlinking.py

--- LEDBlinking.py
+++ LEDBlinking.py
@@ -3,17 +3,21 @@ from time import sleep
 
 LED = 16
 BUTTON = 25
-
 GPIO.setmode(GPIO.LE_POTATO_LOOKUP)
+#Sets up the LED and BUTTON
 GPIO.setup(LED, GPIO.OUT)
 GPIO.setup(BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
+#Runs loop forever 
 while True:
+    #Turns on LED
     GPIO.output(LED, GPIO.HIGH)
+    #If Pressed, blinks for .1 seconds
     if GPIO.input(BUTTON) == GPIO.HIGH:
         sleep(.1)
         GPIO.output(LED, GPIO.LOW)
         sleep(.1)
+    #If NOT Pressed, blinks for .5 seconds
     else:
         sleep(.5)
         GPIO.output(LED, GPIO.LOW)
